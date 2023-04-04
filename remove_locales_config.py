3b6803cc4000840a42237a02e463b71f4bc591f1
Eric Reed <eric@an0n.dev>
2023-04-04 15:58:51 -0400

    Don't choke if no locales config is present

--- remove_locales_config.py
+++ remove_locales_config.py
@@ -4,7 +4,7 @@ import bs4
 base = pathlib.Path ("base")
 
 bad_xml = base / "res" / "xml" / "locales_config.xml"
-os.remove (bad_xml)
+if bad_xml.exists(): os.remove (bad_xml)
 
 values_xml_path = base / "res" / "values" / "public.xml"
 
@@ -15,12 +15,12 @@ for public_tag in resources.find_all ("public"):
     if public_tag ["name"] != "locales_config": continue
     public_tag.decompose ()
     break
-else: raise Exception ("locales_config key not found")
+# else: raise Exception ("locales_config key not found")
 with open (values_xml_path, "w") as values_file_out: values_file_out.write (values_xml.prettify ())
 
 android_manifest_xml_path = base / "AndroidManifest.xml"
 with open (android_manifest_xml_path, "r") as android_manifest_file: android_manifest_xml_text = android_manifest_file.read ()
 android_manifest_xml = bs4.BeautifulSoup (android_manifest_xml_text, "xml")
 application = android_manifest_xml.manifest.application
-del application ["android:localeConfig"]
+if "android:localeConfig" in application: del application ["android:localeConfig"]
 with open (android_manifest_xml_path, "w") as android_manifest_file_out: android_manifest_file_out.write (android_manifest_xml.prettify ())
